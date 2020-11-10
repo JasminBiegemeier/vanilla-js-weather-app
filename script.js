2aa7a2c580d545712fbb5b8e9c99f0744ce5236d
Jasmin Biegemeier <jasmin.biegemeier@enpal.de>
2020-11-10 19:54:27 +0100

    finished weather app

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const baseUrl = "https://api.openweathermap.org/data/2.5/weather"
 // This function only paints stuff on the screen
 function render(data) {
     console.log(data)
+    const date = new Date()
     document.getElementById("location").innerText = data.name
     document.getElementById("temp").innerText = data.main.temp
     document.getElementById("weather").innerText = data.weather[0].main
+    document.getElementById("date").innerText = buildDate()
 
     // here we write the conditional
     if (data.main.temp >= 23) {
@@ -28,5 +30,35 @@ searchBar.addEventListener("keypress", function (event) {
     }
 });
 
-
-
+function buildDate() {
+    let d = new Date();
+    var months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    let day = days[d.getDay()];
+    let month = months[d.getMonth()];
+    // this was easy
+    let date = d.getDate();
+    let year = d.getFullYear();
+    return `${day} ${date} ${month} ${year}`;
+}
